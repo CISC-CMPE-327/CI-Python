a5da3d1cef6dd04b5fccc022d37ac5f1a462d905
Steven Ding <steven.h.ding@mail.mcgill.ca>
2019-10-28 21:36:27 -0400

    updated to an easier example (same in/outs) but different specs
    Signed-off-by: Steven Ding <steven.h.ding@mail.mcgill.ca>

--- qa327/app.py
+++ qa327/app.py
@@ -3,19 +3,29 @@ import os
 
 
 def main():
-    """ An example program that has two functionalities:
-        R1/ Ask for user's name and print it out
-        R2/ Write 'hello 327' to the file specified by app
-            argument if the user's name is 327.
+    """ An example program of frontend that does
+    R1 program only accepts 'login' as key
+    R2 print valid_account_list_file's content
+    R3 write 'hmm i am a transaction.' to the transaction_summary_file
     """
-    print('hello world!')
+    print('Welcome the Queens ATM machine')
+
+    # for simplicity
+    # you can use argparse for sure
+    valid_account_list_file = sys.argv[1]
+    transaction_summary_file = sys.argv[2]
 
     # R1:
-    name = input('what is your name please?:' + os.linesep)
-    print('hello', name)
-
-    # R2:
-    if name == '327':
-        with open(sys.argv[1], 'w') as of1:
-            of1.write('hello 327')
-        print('file written!')
+    user_input = input('what is the key?\n')
+    if(user_input == 'login'):
+        print('here is the content')
+        with open(valid_account_list_file) as rf:
+            print(rf.read())
+        print('writing transactions...')
+        with open(transaction_summary_file, 'w') as wf:
+            wf.write('hmm i am a transaction.')
+            # exit
+    else:
+        print('omg wrong key')
+    
+    
